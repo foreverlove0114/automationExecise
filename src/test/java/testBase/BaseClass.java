b075ab194efbfb24c69201a0a863435d500daedf
Yu Jie Xiang <foreverlove0114@example.com>
2026-01-18 22:48:11 +0800

    first commit

---
+++ src/test/java/testBase/BaseClass.java
@@ -0,0 +1,27 @@
+package testBase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseClass {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://automationexercise.com/");
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
